240027f8e994a6ff956664c5042393dc680e7060
CoenvdStigchel <89966318+CoenvdStigchel@users.noreply.github.com>
2021-12-21 13:37:09 +0100

    Add files via upload

--- RockPaperSicsors.py
+++ RockPaperSicsors.py
@@ -1,27 +1,40 @@
 import random
 
-user = input("Enter a choice (rock, paper, scissors): ")
-keuzes = ["rock", "paper", "scissors"]
-computer= random.choice(keuzes)
-print(f'\nJij kiest >> {user}, computer koos >> {computer}.\n')
+while True:
+    user = input("Enter a choice (steen, papier, schaar): ")
+    keuzes = ["steen","steen","steen", "papier","papier","papier", "schaar","schaar","schaar"]
+    computer= random.choice(keuzes)
+    if not user.lower() in ["steen", "papier", "schaar"]:
+        print("Is geen optie")
+        continue
+    print(f'\nJij kiest >> {user}, computer koos >> {computer}.\n')
 
-if user == computer:
-    print(f"Both players selected {user}. Ooh een gelijkspel!")
-elif user == "rock":
-    if computer == "scissors":
-        print("Rock wint van scissors! Je hebt Gewonnen!")
-    else:
-        print("Paper verliest van rock! Je hebt verloren...")
-elif user == "paper":
-    if computer == "rock":
-        print("Paper wint van rock! Je hebt Gewonnen!")
-    else:
-        print("Scissors cuts paper! Je hebt Verloren...")
-elif user == "scissors":
-    if computer == "paper":
-        print("Scissors wint van  paper! Je hebt Gewonnen!")
-    else:
-        print("Rock wint van scissors! Je hebt Verloren...")
+    if user == computer:
+        print(f"Allebei de spelers gebruikte {user}. Ooh een gelijkspel!")
+    elif user == "steen":
+        if computer == "schaar":
+            print("Steen wint van schaar! Je hebt Gewonnen!")
+        else:
+            print("Paper wint van rock! Je hebt Gewonnen!")
+    elif user == "papier":
+        if computer == "steen":
+            print("Papier wint van steen! Je hebt Gewonnen!")
+        else:
+            print("schaar wint van papier! Je hebt Verloren...")
+    elif user == "schaar":
+        if computer == "papier":
+            print("Schaar wint van  papier! Je hebt Gewonnen!")
+        else:
+            print("Steen wint van schaar! Je hebt Verloren...")
+       
 
-if not user.lower() in ["rock", "paper", "scissors"]:
-    print("Is geen optie")
+    if input("Wil je nog een keer spelen Y/N >> ") == "y":
+        continue
+    else:
+        exit()
+
+
+
+
+
+
